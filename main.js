bfc2e6176591ca7c805699c4ca71e71169526402
Pässword <brunnertho@mediamarktsaturn.de>
2023-11-28 12:42:58 +0100

    Alle Researches

--- main.js
+++ main.js
@@ -252,6 +252,237 @@ document.querySelector("#research2").addEventListener("click", function () {
   }
 });
 
+
+let costDouble      = 1;
+let progressDouble  = 0;
+let timerDouble     = 0;
+
+document.querySelector("#research3").addEventListener("click", function () {  
+  if (score >= costDouble){   
+    score = score - costDouble;  
+    let countdownTimerDouble = setInterval(function() {
+      timerDouble++;
+      let progressDouble = (timerDouble / 1000 /* Hier auch Zeit in 5/1000 einer Sekunde eingeben */) * 100 +"%";
+      document.querySelector("#researchbar3").style.width = progressDouble;
+      
+      if (timerDouble === 1000) {   //Hier Zeit in 5/1000 einer Sekunde eingeben :)
+        clearInterval(countdownTimerDouble);
+        timerDouble = 0;
+        // Ziel Hierrein //
+
+
+
+
+
+        /*              */   
+        document.querySelector("#researchbar3").style.width = 0;
+        document.documentElement.style.setProperty("--research3-color", "rgb(51, 221, 85)");
+    document.querySelector("#research3").style.backgroundColor = "var(--research3-color)";
+    setTimeout(() => {
+      document.documentElement.style.setProperty("--research3-color", "rgb(144, 247, 247)");
+      document.querySelector("#research3").style.backgroundColor = "var(--research3-color)";
+    }, 400);
+      }
+    }, 5);  
+  } else {
+    document.documentElement.style.setProperty("--research3-color", "red");
+    document.querySelector("#research3").style.backgroundColor = "var(--research3-color)";
+    setTimeout(() => {
+      document.documentElement.style.setProperty("--research3-color", "rgb(144, 247, 247)");
+      document.querySelector("#research3").style.backgroundColor = "var(--research3-color)";
+    }, 1500);
+  }
+});
+
+let costRandomizer      = 1;
+let progressRandomizer  = 0;
+let timerRandomizer     = 0;
+
+document.querySelector("#research4").addEventListener("click", function () {  
+  if (score >= costRandomizer){   
+    score = score - costRandomizer;  
+    let countdownTimerRandomizer = setInterval(function() {
+      timerRandomizer++;
+      let progressRandomizer = (timerRandomizer / 1000 /* Hier auch Zeit in 5/1000 einer Sekunde eingeben */) * 100 +"%";
+      document.querySelector("#researchbar4").style.width = progressRandomizer;
+      
+      if (timerRandomizer === 1000) {   //Hier Zeit in 5/1000 einer Sekunde eingeben :)
+        clearInterval(countdownTimerRandomizer);
+        timerRandomizer = 0;
+        // Ziel Hierrein //
+
+
+
+
+
+        /*              */   
+        document.querySelector("#researchbar4").style.width = 0;
+        document.documentElement.style.setProperty("--research4-color", "rgb(51, 221, 85)");
+    document.querySelector("#research4").style.backgroundColor = "var(--research4-color)";
+    setTimeout(() => {
+      document.documentElement.style.setProperty("--research4-color", "rgb(144, 247, 247)");
+      document.querySelector("#research4").style.backgroundColor = "var(--research4-color)";
+    }, 400);
+      }
+    }, 5);  
+  } else {
+    document.documentElement.style.setProperty("--research4-color", "red");
+    document.querySelector("#research4").style.backgroundColor = "var(--research4-color)";
+    setTimeout(() => {
+      document.documentElement.style.setProperty("--research4-color", "rgb(144, 247, 247)");
+      document.querySelector("#research4").style.backgroundColor = "var(--research4-color)";
+    }, 1500);
+  }
+});
+
+
+let costGoofyAh      = 1;
+let progressGoofyAh  = 0;
+let timerGoofyAh     = 0;
+
+document.querySelector("#research5").addEventListener("click", function () {  
+  if (score >= costGoofyAh){   
+    score = score - costGoofyAh;  
+    let countdownTimerGoofyAh = setInterval(function() {
+      timerGoofyAh++;
+      let progressGoofyAh = (timerGoofyAh / 1000 /* Hier auch Zeit in 5/1000 einer Sekunde eingeben */) * 100 +"%";
+      document.querySelector("#researchbar5").style.width = progressGoofyAh;
+      
+      if (timerGoofyAh === 1000) {   //Hier Zeit in 5/1000 einer Sekunde eingeben :)
+        clearInterval(countdownTimerGoofyAh);
+        timerGoofyAh = 0;
+        // Ziel Hierrein //
+
+
+
+
+
+        /*              */   
+        document.querySelector("#researchbar5").style.width = 0;
+        document.documentElement.style.setProperty("--research5-color", "rgb(51, 221, 85)");
+    document.querySelector("#research5").style.backgroundColor = "var(--research5-color)";
+    setTimeout(() => {
+      document.documentElement.style.setProperty("--research5-color", "rgb(144, 247, 247)");
+      document.querySelector("#research5").style.backgroundColor = "var(--research5-color)";
+    }, 400);
+      }
+    }, 5);  
+  } else {
+    document.documentElement.style.setProperty("--research5-color", "red");
+    document.querySelector("#research5").style.backgroundColor = "var(--research4-color)";
+    setTimeout(() => {
+      document.documentElement.style.setProperty("--research5-color", "rgb(144, 247, 247)");
+      document.querySelector("#research5").style.backgroundColor = "var(--research5-color)";
+    }, 1500);
+  }
+});
+
+let costTest      = 1;
+let progressTest  = 0;
+let timerTest     = 0;
+
+document.querySelector("#research6").addEventListener("click", function () {  
+  if (score >= costTest){   
+    score = score - costTest;  
+    let countdownTimerTest = setInterval(function() {
+      timerTest++;
+      let progressTest = (timerTest / 1000 /* Hier auch Zeit in 5/1000 einer Sekunde eingeben */) * 100 +"%";
+      document.querySelector("#researchbar6").style.width = progressTest;
+      
+      if (timerTest === 1000) {   //Hier Zeit in 5/1000 einer Sekunde eingeben :)
+        clearInterval(countdownTimerTest);
+        timerTest = 0;
+        // Ziel Hierrein //
+
+
+
+
+
+        /*              */   
+        document.querySelector("#researchbar6").style.width = 0;
+        document.documentElement.style.setProperty("--research6-color", "rgb(51, 221, 85)");
+    document.querySelector("#research6").style.backgroundColor = "var(--research6-color)";
+    setTimeout(() => {
+      document.documentElement.style.setProperty("--research6-color", "rgb(144, 247, 247)");
+      document.querySelector("#research6").style.backgroundColor = "var(--research6-color)";
+    }, 400);
+      }
+    }, 5);  
+  } else {
+    document.documentElement.style.setProperty("--research6-color", "red");
+    document.querySelector("#research6").style.backgroundColor = "var(--research6-color)";
+    setTimeout(() => {
+      document.documentElement.style.setProperty("--research6-color", "rgb(144, 247, 247)");
+      document.querySelector("#research6").style.backgroundColor = "var(--research6-color)";
+    }, 1500);
+  }
+});
+
+
+
+let costSabotageHouse      = 1;
+let progressSabotageHouse  = 0;
+let timerSabotageHouse     = 0;
+
+document.querySelector("#research7").addEventListener("click", function () {  
+  if (score >= costSabotageHouse){   
+    score = score - costSabotageHouse;  
+    let countdownTimerSabotageHouse = setInterval(function() {
+      timerSabotageHouse++;
+      let progressSabotageHouse = (timerSabotageHouse / 1000 /* Hier auch Zeit in 5/1000 einer Sekunde eingeben */) * 100 +"%";
+      document.querySelector("#researchbar7").style.width = progressSabotageHouse;
+      
+      if (timerSabotageHouse === 1000) {   //Hier Zeit in 5/1000 einer Sekunde eingeben :)
+        clearInterval(countdownTimerSabotageHouse);
+        timerSabotageHouse = 0;
+        // Ziel Hierrein //
+
+
+
+
+
+        /*              */   
+        document.querySelector("#researchbar7").style.width = 0;
+        document.documentElement.style.setProperty("--research7-color", "rgb(51, 221, 85)");
+    document.querySelector("#research7").style.backgroundColor = "var(--research7-color)";
+    setTimeout(() => {
+      document.documentElement.style.setProperty("--research7-color", "rgb(144, 247, 247)");
+      document.querySelector("#research7").style.backgroundColor = "var(--research7-color)";
+    }, 400);
+      }
+    }, 5);  
+  } else {
+    document.documentElement.style.setProperty("--research7-color", "red");
+    document.querySelector("#research7").style.backgroundColor = "var(--research7-color)";
+    setTimeout(() => {
+      document.documentElement.style.setProperty("--research7-color", "rgb(144, 247, 247)");
+      document.querySelector("#research7").style.backgroundColor = "var(--research7-color)";
+    }, 1500);
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let wiederholung = setInterval(() => {
   document.querySelector("#score").textContent = score + " $";
 }, 1);
