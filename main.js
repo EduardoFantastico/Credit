5c3641dd74a77d1d98472ef1441f67a0d9c0c4e9
Eddy <eddyheinz@icloud.com>
2023-11-28 11:15:58 +0100

    Title Screen

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 "use strict";
+document.getElementById("startButton").addEventListener("click", function () {
+  var name = document.getElementById("nameInput").value;
+  if (name) {
+    document.getElementById("startScreen").style.display = "none";
+    document.getElementById("gameScreen").style.display = "block";
+    // Ihr Spielstartcode geht hier, z.B. initGame(name);
+  } else {
+    alert("Bitte geben Sie Ihren Namen ein");
+  }
+});
 
 let level = 0;
 let score = 0;
