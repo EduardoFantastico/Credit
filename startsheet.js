a03d75c107264bd3c1d9b1794f0661c504f00dbf
Eddy <eddyheinz@icloud.com>
2023-11-29 14:27:56 +0100

    changes

--- startsheet.js
+++ startsheet.js
@@ -1,10 +1,24 @@
-/* document.getElementById("startButton").addEventListener("click", function () {
+document.getElementById("startButton").addEventListener("click", function () {
   var name = document.getElementById("nameInput").value;
-  if (name) {
+  document.getElementById("startScreen").style.display = "none";
+  document.getElementById("gameScreen").style.display = "block";
+  // Ihr Spielstartcode geht hier, z.B. initGame(name);
+});
+
+/*
+
+----- SICHERE METHODE ZUM EINLOGGEN
+
+document.getElementById("startButton").addEventListener("click", function () {
+  var name = document.getElementById("nameInput").value;
+  var password = document.getElementById("passwordInput").value;
+  if (name && password === "123") {
     document.getElementById("startScreen").style.display = "none";
     document.getElementById("gameScreen").style.display = "block";
     // Ihr Spielstartcode geht hier, z.B. initGame(name);
   } else {
-    alert("Bitte geben Sie Ihren Namen ein");
+    alert("Bitte geben Sie Ihren Namen und das korrekte Passwort ein");
   }
 });
+
+*/
