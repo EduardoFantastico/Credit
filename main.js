6fc8298fa97f62e09ef864bba27c967b2715ac28
Pässword <brunnertho@mediamarktsaturn.de>
2023-11-29 09:49:09 +0100

    Nur einmal klicken erlaubt

--- main.js
+++ main.js
@@ -272,8 +272,9 @@ let progressLevel = 0;
 let timerLevel = 0;
 let wirdGeupgradedLevel = false;
 
+
 document.querySelector("#research1").addEventListener("click", function () {
-  if (wirdGeupgradedLevel === false) {
+  if (wirdGeupgradedLevel === false) { // tested ob bereits geupdatet wird, damit man nicht 2 mal das selbe gleichzeitig updaten kann
     if (score >= costLevel) {
       wirdGeupgradedLevel = true;
       score = score - costLevel;
@@ -283,7 +284,7 @@ document.querySelector("#research1").addEventListener("click", function () {
         document.querySelector("#researchbar1").style.width = progressLevel;
         // Änderung: Überprüfe, ob timerLevel gleich 1000 ist
         if (timerLevel === 1000) {
-          wirdGeupgradedLevel = false;
+          wirdGeupgradedLevel = false;   //stellt "wird geupdatet" wieder auf false 
           document.querySelector("#researchbar1").style.width = 0;
           timerLevel = 0;
           clearInterval(countdownTimerLevel);
@@ -327,9 +328,13 @@ document.querySelector("#research1").addEventListener("click", function () {
 let costGoldRush = 1;
 let progressGoldRush = 0;
 let timerGoldRush = 0;
+let wirdGeupgradedGoldRush = false;
+
 
 document.querySelector("#research2").addEventListener("click", function () {
+  if (wirdGeupgradedGoldRush === false){
   if (score >= costGoldRush) {
+    wirdGeupgradedGoldRush = true;
     score = score - costGoldRush;
     let countdownTimerGoldRush = setInterval(function () {
       timerGoldRush++;
@@ -378,14 +383,17 @@ document.querySelector("#research2").addEventListener("click", function () {
         "var(--research2-color)";
     }, 1500);
   }
-});
+}});
 
 let costDouble = 1;
 let progressDouble = 0;
 let timerDouble = 0;
+let wirdGeupgradedDouble = false;
 
 document.querySelector("#research3").addEventListener("click", function () {
+  if (wirdGeupgradedDouble === false) {
   if (score >= costDouble) {
+    wirdGeupgradedDouble = true;
     score = score - costDouble;
     let countdownTimerDouble = setInterval(function () {
       timerDouble++;
@@ -434,14 +442,17 @@ document.querySelector("#research3").addEventListener("click", function () {
         "var(--research3-color)";
     }, 1500);
   }
-});
+}});
 
 let costRandomizer = 1;
 let progressRandomizer = 0;
 let timerRandomizer = 0;
+let wirdGeupgradedrandomizer = false;
 
 document.querySelector("#research4").addEventListener("click", function () {
+  if (wirdGeupgradedrandomizer === false) {
   if (score >= costRandomizer) {
+    wirdGeupgradedrandomizer = true;
     score = score - costRandomizer;
     let countdownTimerRandomizer = setInterval(function () {
       timerRandomizer++;
@@ -490,14 +501,17 @@ document.querySelector("#research4").addEventListener("click", function () {
         "var(--research4-color)";
     }, 1500);
   }
-});
+}});
 
 let costGoofyAh = 1;
 let progressGoofyAh = 0;
 let timerGoofyAh = 0;
+let wirdGeupgradedGoofyAh = false;
 
 document.querySelector("#research5").addEventListener("click", function () {
+  if (wirdGeupgradedGoofyAh === false){
   if (score >= costGoofyAh) {
+    wirdGeupgradedGoofyAh = true;
     score = score - costGoofyAh;
     let countdownTimerGoofyAh = setInterval(function () {
       timerGoofyAh++;
@@ -546,14 +560,17 @@ document.querySelector("#research5").addEventListener("click", function () {
         "var(--research5-color)";
     }, 1500);
   }
-});
+}});
 
 let costTest = 1;
 let progressTest = 0;
 let timerTest = 0;
+let wirdGeupgradedTest = false;
 
 document.querySelector("#research6").addEventListener("click", function () {
+  if (wirdGeupgradedTest === false){
   if (score >= costTest) {
+    wirdGeupgradedTest = true;
     score = score - costTest;
     let countdownTimerTest = setInterval(function () {
       timerTest++;
@@ -602,14 +619,17 @@ document.querySelector("#research6").addEventListener("click", function () {
         "var(--research6-color)";
     }, 1500);
   }
-});
+}});
 
 let costSabotageHouse = 1;
 let progressSabotageHouse = 0;
 let timerSabotageHouse = 0;
+let wirdGeupgradedSabotageHouse = false;
 
 document.querySelector("#research7").addEventListener("click", function () {
+  if (wirdGeupgradedSabotageHouse === false) {
   if (score >= costSabotageHouse) {
+    wirdGeupgradedSabotageHouse = true;
     score = score - costSabotageHouse;
     let countdownTimerSabotageHouse = setInterval(function () {
       timerSabotageHouse++;
@@ -659,7 +679,7 @@ document.querySelector("#research7").addEventListener("click", function () {
         "var(--research7-color)";
     }, 1500);
   }
-});
+}});
 
 let scorechecker = setInterval(() => {
   if (score > levelLimits[level]) {
