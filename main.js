da7e9e005c55a6233df1573afc6be61233ed466c
Eddy <eddyheinz@icloud.com>
2023-12-18 09:13:26 +0100

    cgh

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ let level = 0;
 let score = 0;
 let pointsPerClick = 1; // Punkte pro Klick
 let levelLimits = [
-  100,
-  500,
   1000,
-  2500,
   5000,
   10000,
   25000,
   50000,
   100000,
+  250000,
+  500000,
   1000000,
+  10000000,
   Infinity,
 ];
 
