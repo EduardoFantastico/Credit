a65f9ce2896f2933ea4f53a640bd307796b1976d
Eddy <eddyheinz@icloud.com>
2024-01-23 15:53:03 +0100

    zucker

--- startsheet.js
+++ startsheet.js
@@ -3,7 +3,10 @@ window.onload = function () {
 };
 
 document.getElementById("multiButton").addEventListener("click", function () {
-  let name = document.getElementById("nameInput").value;
   document.getElementById("startscreen").style.display = "none";
   document.getElementById("loginScreen").style.display = "block";
+});
+
+document.getElementById("quitButton").addEventListener("click", function () {
+  window.close();
 });
