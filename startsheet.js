581d5de1dc143d4223d413872c4f5f27e7fa8e5d
Eddy <eddyheinz@icloud.com>
2024-01-23 16:48:30 +0100

    backpulver

--- startsheet.js
+++ startsheet.js
@@ -7,6 +7,13 @@ document.getElementById("multiButton").addEventListener("click", function () {
   document.getElementById("loginScreen").style.display = "block";
 });
 
+document.getElementById("singleButton").addEventListener("click", function () {
+  document.getElementById("startscreen").style.display = "none";
+  document.getElementById("loginScreen").style.display = "none";
+  document.getElementById("gameScreen").style.display = "block";
+  document.getElementById("myAudio").pause();
+});
+
 document.getElementById("quitButton").addEventListener("click", function () {
   window.close();
 });
