27e49e0e3de7692ef792610c6c49285c1f0ad853
Pässword <brunnertho@mediamarktsaturn.de>
2024-01-24 13:44:00 +0100

    Python wieder entfernt

--- loginsheet.js
+++ loginsheet.js
@@ -2,6 +2,57 @@ document
   .getElementById("loginStartButton")
   .addEventListener("click", function () {
     let name = document.getElementById("nameInput").value;
+    document.getElementById("loginScreen").style.display = "none";
+    document.getElementById("gameScreen").style.display = "block";
+    // Ihr Spielstartcode geht hier, z.B. initGame(name);
+
+    // Unterbrechen Sie die Wiedergabe des Audios
+    document.getElementById("myAudio").pause();
+  });
+
+document
+  .getElementById("registerButton")
+  .addEventListener("click", function () {
+    document.getElementById("loginScreen").style.display = "none";
+    document.getElementById("registerScreen").style.display = "block";
+  });
+
+// ----- SICHERE METHODE ZUM EINLOGGEN
+
+document
+  .getElementById("loginStartButton")
+  .addEventListener("click", function () {
+    let name = document.getElementById("nameInput").value;
+    let password = document.getElementById("passwordInput").value;
+    if (name && password === "123") {
+      document.getElementById("loginScreen").style.display = "none";
+      document.getElementById("gameScreen").style.display = "block";
+      // Ihr Spielstartcode geht hier, z.B. initGame(name);
+    } else {
+      alert("Bitte geben Sie Ihren Namen und das korrekte Passwort ein");
+    }
+  });
+
+document
+  .getElementById("loginBackButton")
+  .addEventListener("click", function () {
+    document.getElementById("loginScreen").style.display = "none";
+    document.getElementById("startscreen").style.display = "block";
+
+    // Überprüfen Sie, ob die CSS-Stile korrekt angewendet werden
+    console.log(
+      window.getComputedStyle(document.getElementById("startscreen"))
+    );
+
+    // Überprüfen Sie, ob es andere JavaScript-Funktionen gibt, die die Startseite beeinflussen
+    // Fügen Sie Ihren Code hier ein
+  });
+
+
+/* document
+  .getElementById("loginStartButton")
+  .addEventListener("click", function () {
+    let name = document.getElementById("nameInput").value;
     let password = document.getElementById("passwordInput").value;
     socket.send(
       JSON.stringify({
@@ -42,3 +93,4 @@ document
     // Überprüfen Sie, ob es andere JavaScript-Funktionen gibt, die die Startseite beeinflussen
     // Fügen Sie Ihren Code hier ein
   });
+*/
