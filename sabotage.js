eaaca53ef3014d93e95da135eda7a8545d85e010
P4ss0w0rd <p4sword@web.de>
2024-02-09 14:51:40 +0100

    pp

--- sabotage.js
+++ sabotage.js
@@ -1,14 +1,12 @@
 let sabotages = [];
-let openWindowId = null;
 
-function addSabotage(id, displayName, cost, producetime, usetime) {
+function addSabotage(id, displayName, cost, producetime) {
   const newSabotage = {
     id: id,
     displayName: displayName,
     cost: cost,
     uses: 0,
     producetime: producetime,
-    usetime: usetime,
     inUse: false,
     inDevelopment: false,
   };
@@ -36,48 +34,9 @@ function createSabotageElement(sabotage) {
     SabotageElement.style.justifyContent = "center"; // Zentriert den Inhalt horizontal
   }
 
-  // Fügt einen Event-Listener zum neuen Element hinzu
-  SabotageElement.addEventListener("click", function() {
-    openWindow(sabotage);
-  });
-
   // Fügt das neue Element zur "content-box-sabotage" hinzu
   document.getElementById("content-box-sabotage").appendChild(SabotageElement);
 }
-
-
-function openWindow(sabotage) {
-  // Überprüft, ob das Fenster, das geöffnet werden soll, bereits geöffnet ist
-  if (openWindowId === "window-" + sabotage.id) {
-    // Wenn ja, entfernt es
-    document.getElementById(openWindowId).remove();
-    openWindowId = null;
-    return;
-  }
-
-  // Überprüft, ob bereits ein anderes Fenster geöffnet ist
-  if (openWindowId) {
-    // Wenn ja, entfernt es
-    document.getElementById(openWindowId).remove();
-  }
-
-  // Erstellt ein neues Fenster
-  let window = document.createElement("div");
-  window.id = "window-" + sabotage.id;
-  window.classList.add("sabotageWindow"); // Fügt die CSS-Klasse hinzu
-
-  // Fügt das Fenster zum Dokument hinzu
-  document.body.appendChild(window);
-
-  // Speichert die ID des geöffneten Fensters
-  openWindowId = window.id;
-}
-
-
-
-
-
-
 
 addSabotage('sabotage1', 'Operation Blitz', 100, 60);
 addSabotage('sabotage2', 'Mission Phantom', 200, 120);
